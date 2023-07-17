ce8529c7828cd817b8082506bce15a8c11969e1b
enriqueGaleanoT <enriquej999@gmail.com>
2023-07-17 17:11:16 -0600

    TO-DO add more things

--- data.js
+++ data.js
@@ -1,15 +1,30 @@
 function driverProgram(){
     let contactList = ["Enrique Galeano", "ADA School", "Cristobal Colon", "Elizabeth Marie"];
+    console.log("1. Lista original:"+ contactList);
 
-    let testContact = "Hola Me como Los mocos"
-
-   console.log(addContact(contactList, testContact));
+    let addContacts = "Carlos Jose" 
     
+    console.log("2. Lista con un contacto nuevo: ");
+    addContactToList(contactList, addContacts); 
+
+    console.log("3. Elemento removido de la lista: ");
+    popContactFromList(contactList);
+
+    printArray(contactList);
 }
 
-function addContact(arrayContact,contact){
-    let newArrayHodler = arrayContact.push(contact);
-    for (let i = 0; i < newArrayHodler.length; i++) {
-        console.log(newArrayHodler[i]);
+function addContactToList(arrayContacts, contact){
+    arrayContacts.push(contact);
+    for (let i = 0; i < arrayContacts.length; i++) {
+        console.log(arrayContacts[i]);
     }
+}
+
+function popContactFromList(arrayContacts){
+    console.log("Elemento:" + arrayContacts.pop());
+
+}
+
+function printArray(arrayContacts){
+    console.log("4. Lista Nueva: "+arrayContacts);
 }
