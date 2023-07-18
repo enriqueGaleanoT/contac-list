d6dc0245b7fbd514f387fb67b917d566b510dd54
Enrique Galeano <enriquej999@gmail.com>
2023-07-18 16:03:24 -0600

    done with exercise

--- data.js
+++ data.js
@@ -1,40 +1,46 @@
-function driverProgram(){
-    // let contactList = ["Enrique Galeano", "ADA School", "Cristobal Colon", "Elizabeth Marie"];
-    // console.log("1. Lista original:"+ contactList);
+const contactList = [
+    {
+        id: 0,
+        name: "Enrique",
+        surname: "Galenao",
+        contact: 47289341,
+        city: "Tegucigalpa",
+        direction: "Col. Hato"
+    }
+]
 
-    // let addContacts = "Carlos Jose" 
-    
-    // console.log("2. Lista con un contacto nuevo: ");
-    // addContactToList(contactList, addContacts); 
-
-    // console.log("3. Elemento removido de la lista: ");
-    // popContactFromList(contactList);
-
-    // printArray(contactList);
-
-    let contactList = [
-        {
-
+function driverAdd(){
+    function addContactToList(ID, nombre, apellidos, telefono, ciudad, dirrecion){
+        let newContact = {
+            id: ID,
+            name: nombre,
+            surname: apellidos,
+            contact: telefono,
+            city: ciudad,
+            direction: dirrecion
         }
-    ]
+        contactList.push(newContact);
+        console.log(newContact);
+    }
+    console.log("Se agregaron estas personas a la lista");
+    addContactToList(1, "Lewis", "Hamilton", 96243, "London", "colonia main street");
+    addContactToList(1, "Sergio", "perez", 12345, "tegucigalpa", "colonia las uvas");
+    addContactToList(1, "fer", "martinez", 54321, "san pedro sula", "colonia 15 de sept");
 }
 
-function addContactToList(ID, nombre, apellidos, telefono, ciudad, dirrecion){
-    let newContact = {
-        id: ID,
-        name: nombre,
-        surname: apellidos,
-        contact: telefono,
-        city: ciudad,
-        direction: dirrecion
+function driverShow(){
+    console.log("Lista: ");
+    for (let i = 0; i < contactList.length; i++) {
+        console.log(contactList[i]);
+    }
+    if (contactList.length === 0) {
+        console.log("La lista de contactos esta vacia");
     }
 }
 
-function popContactFromList(arrayContacts){
-    // console.log("Elemento:" + arrayContacts.pop());
-
+function popContactFromList(){
+    console.log("Se elimino el ultimo elemento de la lista");
+    console.log("Elemento:" + contactList.pop());
 }
 
-function printArray(arrayContacts){
-    // console.log("4. Lista Nueva: "+arrayContacts);
-}
+
